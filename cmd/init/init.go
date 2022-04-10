1c0a41a08959cdd5e982f98f84b0f22b3acb682c
commojun <junpooooow@gmail.com>
2022-04-11 01:00:44 +0900

    ローカルにDLまでできた。

--- cmd/init/init.go
+++ cmd/init/init.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -12,15 +13,47 @@ func main() {
 	root := os.Getenv("DAV_URL")
 	user := os.Getenv("DAV_USER")
 	password := os.Getenv("DAV_PASS")
+	remoteFolder := os.Getenv("REMOTE_FOLDER")
+	localFolder := os.Getenv("LOCAL_FOLDER")
 
 	c := gowebdav.NewClient(root, user, password)
 
-	files, err := c.ReadDir("Picture/イラスト/今日のすなっぴー")
+	files, err := c.ReadDir(remoteFolder)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, file := range files {
 		//notice that [file] has os.FileInfo type
-		fmt.Println(file.Name())
+		name := file.Name()
+		fmt.Println(name)
+		if file.IsDir() {
+			continue
+		}
+
+		remotePath := remoteFolder + "/" + name
+		localPath := localFolder + "/" + name
+
+		// すでにファイルがあればDLしない
+		if fileExists(localPath) {
+			// add to map
+			continue
+		}
+
+		bytes, err := c.Read(remotePath)
+		if err != nil {
+			log.Printf("Could not read %s: %s\n", remotePath, err)
+			continue
+		}
+		err = ioutil.WriteFile(localPath, bytes, 0644)
+		if err != nil {
+			log.Printf("Could not write %s: %s\n", localPath, err)
+			continue
+		}
+		// add to map
 	}
+}
+
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
 }
